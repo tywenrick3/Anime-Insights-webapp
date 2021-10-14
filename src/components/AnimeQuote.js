bbfe299e5cd2d69371a9c333d7442cdeb9db90bc
Ty Wenrick <tew287@nyu.edu>
2021-10-14 09:41:42 -0400

    getting data

---
+++ src/components/AnimeQuote.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function AnimeQuote() {
+	return <div>Quote of The Day</div>;
+}
+
+export default AnimeQuote;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,34 @@
 import React, { useEffect, useState, useMemo } from 'react';
+import axios from 'axios';
+
+const QUERY_STRING = `https://cors-anywhere.herokuapp.com/https://animechan.vercel.app/api/random`;
 
 function Home() {
+	//const [quote, setQuote] = useState();
+
+	fetch(QUERY_STRING)
+		.then((response) => response.json())
+		.catch(function (error) {
+			console.warn(error);
+		})
+		.then((quote) => console.log(quote));
+
+	/*
+	useEffect(() => {
+		if (!quote) {
+			axios
+				.get(QUERY_STRING)
+				.then(function (response) {
+					const animeQuote = response.data;
+					setQuote(animeQuote);
+				})
+				.catch(function (error) {
+					console.warn(error);
+				});
+		}
+	}, []);
+	*/
+
 	return (
 		<main className='App'>
 			<header>
