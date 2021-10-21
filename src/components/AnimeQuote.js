c3de6c0809ed8a440479b4c75ca44e1c9bf478d3
Ty Wenrick <tew287@nyu.edu>
2021-10-21 13:58:26 -0400

    minor

--- src/components/AnimeQuote.js
+++ src/components/AnimeQuote.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-function AnimeQuote() {
-	return <div>Quote of The Day</div>;
+function AnimeQuote({ anime, name, quote }) {
+	return (
+		<section className='AnimeQuote'>
+			<div>
+				<h2>{anime}</h2>
+				<h3>{name}</h3>
+				<p>--"{quote}"</p>
+			</div>
+		</section>
+	);
 }
 
 export default AnimeQuote;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,19 +1,25 @@
 import React, { useEffect, useState, useMemo } from 'react';
 import axios from 'axios';
+import AnimeQuote from '../components/AnimeQuote';
 
 let name = 'luffy';
 const ANIME_QUERY_STRING = `https://cors-anywhere.herokuapp.com/https://animechan.vercel.app/api/quotes/character?name=${name}`;
 
 function Home() {
-	const [quote, setQuote] = useState();
+	const [quoteData, setQuoteData] = useState();
+
+	function getRandomIndex(min, max) {
+		return Math.floor(Math.random() * (max - min + 1)) + min;
+	}
 
 	useEffect(() => {
-		if (!quote) {
+		if (!quoteData) {
 			axios
 				.get(ANIME_QUERY_STRING)
 				.then(function (response) {
-					const quotes = response.data;
-					setQuote(quotes);
+					let index = getRandomIndex(0, 9);
+					const quotes = response.data[index];
+					setQuoteData(quotes);
 					console.log(quotes);
 				})
 				.catch(function (error) {
@@ -22,13 +28,22 @@ function Home() {
 		}
 	}, []);
 
+	const { anime, name, quote } = useMemo(() => {
+		if (!quoteData) return {};
+		return {
+			anime: quoteData.anime,
+			name: quoteData.character,
+			quote: quoteData.quote,
+		};
+	}, [quoteData]);
+
 	return (
 		<main className='App'>
 			<header>
-				<h1>Midterm Project</h1>
+				<h1>Get inspired by your favorite anime character</h1>
 				<nav className='navbar'></nav>
 			</header>
-
+			<AnimeQuote anime={anime} name={name} quote={quote} />
 			<footer>
 				<a
 					href='https://cors-anywhere.herokuapp.com/corsdemo'
